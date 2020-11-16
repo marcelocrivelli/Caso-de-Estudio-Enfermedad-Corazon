8531caab0cea4f452b15c9765f3bf917262cbaa1
marcelocrivelli <crivellimarcelo@gmail.com>
2020-11-16 13:56:15 -0300

    Add files via upload

---
+++ orderdata.py
@@ -0,0 +1,34 @@
+fileName = "switzerland"
+
+f = open(fileName + '.data', "r", errors='ignore')
+
+lines = 9
+data = []
+row = ""
+for line in f:
+	splt = line.split(' ')
+	for s in splt:
+		if '\n' in s:
+			s = s.replace('\n','')
+		row += s + ' '
+	if lines > 0:
+		lines -= 1
+	else:
+		# Sin el espacio final
+		data.append(row[:-1])
+		row = ""
+		lines = 9
+
+f.close()
+# print(data)
+
+out = open(fileName + "final.data", "w")
+
+for i in data:
+	out.write(i + '\n')
+	# if i.split(' ')[0] == '298':
+	# 	break
+
+print(len(data))
+
+out.close()
